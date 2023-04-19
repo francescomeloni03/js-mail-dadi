1a3afddaeea4c724e80eef11cf3c5924d6136884
Francesco Meloni <francesco.meloni03@icloud.com>
2023-04-19 10:29:32 +0200

    es

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ console.log(`l'email dell'utente è ${email}`)
 const utenti = ["francesco@gmail.com","luca@gmail.com","alessia@gmail.com","fabio@gmail.com","simone@gmail.com"]
 
 
-console.log("Lista delle Eamil:")
+console.log("Lista delle Email:")
 
 console.log( utenti[0] )
 console.log( utenti[1] )
@@ -16,18 +16,22 @@ console.log( utenti[3] )
 console.log( utenti[4] )
 
 
-
+let emailNellaLista = false;
 
 for (let c = 0; c < 5; c++ ) {
 
     if ( utenti[c] == email){
         console.log("L'email è nella lista. L'utente puo accedere.");
+        emailNellaLista = true;
     }
-
-     else {
-         console.log("L'email non è nella lista. L'utente non puo accedere")
-     }
 }
+
+if (emailNellaLista != true) {
+    console.log("L'email non è nella lista. L'utente non puo accedere.");
+}
+
+
+
 
 
    
